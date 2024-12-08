9c9a7364b2ca13979c21841e9f27b0d3ce08c0a0
MarkLuck369 <markplaysgames369@gmail.com>
2024-12-09 06:24:11 +0800

    Initial commit

---
+++ scene/collectable.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+@onready var game_manager: Node = %GameManager
+
+
+func _on_body_entered(body):
+	if (body.name == "CharacterBody2D"):
+		$gura.play()
+		await $gura.finished
+		queue_free()
+		game_manager.add_point()
